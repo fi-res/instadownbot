9ea06400869cfbda7eefc9f539ceba86c91b8fab
fi-res <nta16022013@gmail.com>
2026-05-11 20:25:34 +0300

    init commit

---
+++ lib.py
@@ -0,0 +1,50 @@
+from subprocess import run
+from pathlib import Path
+from shutil import rmtree
+from asyncio import sleep
+
+from aiogram.types import Message, BufferedInputFile, InputMediaPhoto, MediaUnion
+
+def download(url: str):
+    code = url.split('?')[0].rstrip('/').rsplit('/', maxsplit=1)[-1]
+    print('download', code)
+    out = run(['uv', 'run', 'instaloader', '--no-metadata-json', '--no-compress-json', '--no-video-thumbnails', '--dirname-pattern', 'out', '--', f'-{code}'], capture_output=True, text=True).stderr
+    print('out:', out)
+    if 'Errors or warnings occurred:\n' in out:
+        print('error:', out.split('Errors or warnings occurred:\n')[1])
+        return out.split('Errors or warnings occurred:\n')[1]
+
+async def reply(message: Message):
+    path = Path('out')
+
+    try:
+        caption = None
+        if list(path.glob('*.txt')):
+            caption = next(path.glob('*.txt')).read_text()
+
+        if list(path.glob('*.mp4')):
+            print('reply video')
+            video = next(path.glob('*.mp4'))
+            await message.reply_video(BufferedInputFile(video.read_bytes(), video.name), caption=caption)
+            return
+
+        photos = list(path.glob('*.jpg'))
+        if len(photos) == 1:
+            print('reply photo')
+            await message.reply_photo(BufferedInputFile(photos[0].read_bytes(), photos[0].name), caption=caption)
+        else:
+            print('reply media group')
+            media: list[MediaUnion] = [InputMediaPhoto(media=BufferedInputFile(photo.read_bytes(), photo.name), caption=caption if i == 0 else None) for i, photo in enumerate(photos)]
+            # media[0].caption = caption
+            to_send = []
+            for i, photo in enumerate(media):
+                to_send.append(photo)
+                if (i + 1) % 10 == 0:
+                    await message.reply_media_group(to_send, caption=caption)
+                    await sleep(5)
+                    to_send.clear()
+            if to_send:
+                await message.reply_media_group(to_send, caption=caption)
+
+    finally:
+        rmtree(path)
--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from instadownbot!")
+from asyncio import run, CancelledError
+from os import getenv
 
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+
+from lib import download, reply
+
+load_dotenv()
+
+bot = Bot(getenv('TOKEN', ''))
+dp = Dispatcher()
+
+
+@dp.message(F.text.startswith('https://www.instagram.com/'))
+@dp.message(F.text.startswith('https://instagram.com/'))
+async def process(message: Message):
+    assert message.text
+
+    print('process message', message.text)
+    load_message = await message.answer('Загрузка...')
+
+    res = download(message.text)
+    await load_message.delete()
+    if res is not None:
+        await message.reply(f'Ошибка загрузки: {res}')
+        return
+
+    await reply(message)
+
+async def main():
+    print('start polling')
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
+    try:
+        run(main())
+    except (KeyboardInterrupt, CancelledError):
+        print('bot stop (cancelled by user)')
